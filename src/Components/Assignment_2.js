250044b296cc19b1a37376dc1693b042939edc0c
Sintu Kumar <kumarsintu499@gmail.com>
2023-12-10 23:05:45 +0530

    second Assignment

---
+++ src/Components/Assignment_2.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Assignment_2() {
+    return (
+        <div>
+            1. What is Single Page Application (SPA)?
+            Ans:- Reat is a single page application. it's allow to use other page without loading whole pages.
+            2. What is DNS and IP Address?
+            Ans:- DNS- Domain name system.
+            What is DOM in JS?
+            What is  React and why we use React instead of JS
+            What is JSX and why do we use it instead of js?
+            What is package.json?
+            What is the Difference between react and react native? Which one is library or framework?
+            What is difference between class and functional component?
+        </div>
+    )
+}
