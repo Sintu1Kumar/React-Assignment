54f2b7d7805e18e7e98e41ed6fa697dc69ebdbd7
Sintu Kumar <kumarsintu499@gmail.com>
2023-12-14 12:13:57 +0530

    Assignment

--- src/Components/Assignment_2.js
+++ src/Components/Assignment_2.js
@@ -4,28 +4,28 @@ export default function Assignment_2() {
     return (
         <div>
             <h1>React Assignment-2</h1>
-            1. What is Single Page Application (SPA)?
+            <h3>1. What is Single Page Application (SPA)?</h3>
             Ans:- Reat is a single page application. it's allow to use other page without loading whole pages.
 
-            2. What is DNS and IP Address?
+            <h3>2. What is DNS and IP Address?</h3>
             Ans:-DNS:- The Domain Name System (DNS) is the phonebook of the Internet. Humans access information online through domain names, like nytimes.com or espn.com. Web browsers interact through Internet Protocol (IP) addresses. DNS translates domain names to IP addresses so browsers can load Internet resources.
             IP:-The Internet Protocol (IP) is set of rules for routing and addressing packets of data so that they can travel across networks and arrive at the correct destination.
 
-            3. What is DOM in JS?
+            <h3>3. What is DOM in JS?</h3>
             Ans:- The Document Object Model (DOM) is a programming interface for HTML and XML documents. It defines the logical structure of documents and the way a document is accessed and manipulated.
 
-            4. What is  React and why we use React instead of JS.
+            <h3>4. What is  React and why we use React instead of JS.</h3>
             Ans:- ReactJS is a declarative efficient and flexible JavaScript library for building user interfaces. It is an open-source component-based front-end library.
             We use reactjs because it is single page application and it is fast render from react.
 
-            5. What is JSX and why do we use it instead of js?
+            <h3>5. What is JSX and why do we use it instead of js?</h3>
             Ans:- JSX stands for JavaScript XML. JSX allow us to write HTML elements in JavaScript and place them in the DOM without any createElement() or appendChild() methods.
             JSX converts HTML tags into react elements.
 
-            6. What is package.json?
+            <h3>6. What is package.json?</h3>
             Ans:- JSON stands for JavaScript Object Notation. The package.json file contains descriptive and functional metadata about a project such as a name version and dependencies.
 
-            7. What is the Difference between react and react native? Which one is library or framework?
+            <h3>7. What is the Difference between react and react native? Which one is library or framework?</h3>
             Ans:- React is the labuary but react native is the frame work.
         </div>
     )
---
+++ src/Components/Assignment_3.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function Assignment_3() {
+    return (
+        <div>
+            <h1>Assignment-3</h1>
+            1. What is use of Gitignore file in react application?
+            Ans:- It's a best practice to create a .gitignore file with all the files and the different file patterns you want to ignore
+            when you create a new repository before committing it.
+
+            3. what do you know about NPM in react?
+            Ans:- NPM stands for node package manager.
+            The node package manager(NPM) is used for managing and sharing the packages for either React or Angular.
+
+            4. what are the advantages of using React?
+            Ans:- i-React has a Simple mental model.
+
+            ii-Comparison to Js, React is easy to maintain.
+
+            iii-Reusable Components:-
+            One of the main benefits of using React JS is its potential to reuse components. It saves time for developers as
+            they don’t have to write various codes for the same features. Furthermore, if any changes are made in any particular part,
+            it will not affect other parts of the application.
+
+            iv-Flexibility:-
+            Compared to other frontend frameworks, the React code is easier to maintain and is flexible due to its modular structure.
+            This flexibility, in turn, saves a huge amount of time and cost for businesses.
+
+            v-Performance:-
+            React JS was designed to provide high performance in mind. The core of the framework offers a virtual DOM program and
+            server-side rendering, which makes complex apps run extremely fast.
+
+            5. What are the limitations of react?
+            Ans:- Limitations of React JS :-
+            i-Lack of Proper Documentation.
+            ii-Development Speed.
+            iii-JSX Complexity.
+            iv-Problems With SEO
+        </div>
+    )
+}
